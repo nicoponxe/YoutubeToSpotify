8ad48f6552e9e0af02d358635b5fe77d13d006c4
Nicolas Ponce <nicohilton@live.com.ar>
2022-10-08 21:34:53 -0300

    Update SpotifyAPI.java

--- src/main/java/API/SpotifyAPI.java
+++ src/main/java/API/SpotifyAPI.java
@@ -2,17 +2,15 @@ package API;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 public class SpotifyAPI {
 
-    private String authentication; // Generar Token OAuth 2.0
+    private String authentication = "TOKEN AUTH";
     private String id; //ID de usuario en spotify
 
     public SpotifyAPI() {  //Constructor que almaneca en variable el ID del usuario en spotify
@@ -22,7 +20,6 @@ public class SpotifyAPI {
             throw new RuntimeException(e);
         }
     }
-
 
     public  String UsuarioID() throws IOException {  //Recupero JsonObject con info del usuario, devuelvo ID de usuario
 
@@ -34,40 +31,77 @@ public class SpotifyAPI {
 
     public JSONArray Playlists() throws IOException { //Recupero JsonObject con info de playlist y devuelvo JsonArray solo con playlist
 
-        JSONObject playlistInfo = Get("https://api.spotify.com/v1/me/playlists");
+        JSONObject playlistInfo = Get("https://api.spotify.com/v1/me/playlists?limit=49");
         return playlistInfo.getJSONArray("items");
 
     }
 
     public String Cancion(String titulo) throws IOException {  //Devuelvo URI (id) de canción
 
-        String tituloFormateado = titulo.replace(" ","%20");
-
+        String tituloFormateado = URLEncoder.encode(titulo, "UTF-8");  //formateo el titulo en url encoding
+        String cancion = null;
         JSONObject canciones = Get("https://api.spotify.com/v1/search?q="+tituloFormateado+"&type=track&limit=1");
-        String cancion = canciones.getJSONObject("tracks").getJSONArray("items").getJSONObject(0).getString("uri");  //siempre devuelve el primer resultado de la busqueda
+        if (canciones != null){
+             cancion = canciones.getJSONObject("tracks").getJSONArray("items").getJSONObject(0).getString("uri");  //siempre devuelve el primer resultado de la busqueda
+        }
         return cancion;
 
     }
 
     public String CrearPlaylist() throws IOException {   // Agrego cancion a playlist y devuelvo su ID
 
-        Boolean creado = Post("https://api.spotify.com/v1/users/"+this.id+"/playlists");
+        String json = new JSONObject()
+                .put("name", "Songs from Youtube")
+                .put("description", "Canciones de videos de youtube likeados")
+                .put("public", "false")
+                .toString();
 
-        if (creado){
+        Boolean creado = Post("https://api.spotify.com/v1/users/" + this.id + "/playlists", json);
+        String playlistID = null;
+
+        if (creado) {
             JSONArray playlists = Playlists(); // Recorro playlist para encontrar el ID de la nueva playlist
-            String id = playlists.getJSONObject(0).getString("id"); //Devuelvo el ID de la primer playlist ya que es por orden de creación
+            playlistID = playlists.getJSONObject(0).getString("id");
         }
-        return id;
+        return playlistID;
+
     }
+
 
     public boolean AgregarCancionAPlaylist(String cancion, String playlist) throws IOException {   //sobrecarga de metodo para agregar canciones
 
-        return Post("https://api.spotify.com/v1/playlists/"+playlist+"/tracks?uris="+cancion+"");
+        String json = "{\"uris\":[\""+cancion+"\"],\"position\":0}";
+        return Post("https://api.spotify.com/v1/playlists/"+playlist+"/tracks", json);
 
     }
 
-
     //Solicitudes (requests)
+
+
+    private boolean Post(String uri, String data) throws IOException {  //Sobrecarga para realizar post con body
+        URL url = new URL(uri);
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        http.setRequestProperty("Accept", "application/json");
+        http.setRequestProperty("Content-Type", "application/json");
+        http.setRequestProperty("Authorization", authentication);
+
+
+        byte[] out = data.getBytes(StandardCharsets.UTF_8);
+        OutputStream stream = http.getOutputStream();
+        stream.write(out);
+
+        int respuesta = http.getResponseCode();
+        String respuesta2 = http.getResponseMessage();
+
+        if (http.getResponseCode() == 201){ //201 = Creada/agregada
+            http.disconnect();
+            return true;
+        }else {
+            return false;
+        }
+    }
 
     private JSONObject Get(String uri) throws IOException {
 
@@ -78,12 +112,15 @@ public class SpotifyAPI {
         conn.setRequestProperty("Authorization", authentication);
         conn.connect();
 
+        String respuesta = conn.getResponseMessage();
+
         if (conn.getResponseCode() == 200) {
             StringBuffer response = new StringBuffer();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));  //El bufferreader se cierra solo porque adentro tiene un reader, LEER igual.
             String line;
             while ((line = reader.readLine()) != null) {
                 response.append(line);
+                response.append(System.lineSeparator()) ; // agrega separador
             }
             conn.disconnect();
             JSONObject Json = new JSONObject(response.toString());
@@ -95,30 +132,6 @@ public class SpotifyAPI {
         }
     }
 
-    private boolean Post(String uri) throws IOException {
-
-        URL url = new URL(uri);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("POST");
-        conn.setDoOutput(true);
-        conn.setRequestProperty("Accept", "application/json");
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setRequestProperty("Authorization", authentication);
-
-        if (uri.contains("id")){  //Solo se envia un body en caso que se quiera crear una playlist. Sino, solo se agregan canciones.
-
-            String data = "{\"name\":\"Songs from Youtube\",\"description\":\"Canciones de videos de youtube likeados\",\"public\":false}";
-            byte[] out = data.getBytes(StandardCharsets.UTF_8);
-            OutputStream stream = conn.getOutputStream();
-            stream.write(out);
-        }
-
-        if (conn.getResponseCode() == 201){ //201 = Creada/agregada
-            return true;
-        }else {
-            return false;
-        }
-    }
 
 }
 
